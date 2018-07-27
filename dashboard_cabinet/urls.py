26ecf91044976dffeb22290f1317aa9f7d15e3fa
Alekxandr <alkv84@yandex.ru>
2018-07-27 15:44:00 +0300

    1

--- dashboard_cabinet/urls.py
+++ dashboard_cabinet/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import DashboardView
 
-app_name = 'dashboard'
+app_name = 'dashboard_cabinet'
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
 ]
