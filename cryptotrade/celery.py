7101fc475787a146c1da7db89df6434ee0c798a2
Alekxandr <alkv84@yandex.ru>
2018-11-30 01:56:49 +0800

    1

--- cryptotrade/celery.py
+++ cryptotrade/celery.py
@@ -10,15 +10,15 @@ app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'start_block_io_checker-each-5-minutes': {
-        'task': 'finance.start_block_io_checker_task',
+        'task': 'finance.tasks.start_block_io_checker_task',
         'schedule': crontab(minute='*/1')
     },
     'blockio_transfer_usd_to_user_balance-each-5-minutes': {
-        'task': 'finance.blockio_transfer_usd_to_user_balance_task',
+        'task': 'finance.tasks.blockio_transfer_usd_to_user_balance_task',
         'schedule': crontab(minute='*/1')
     },
     'points-awars-each-friday-in-6h-10m': {
-        'task': 'award.start_point_awadr_task',
+        'task': 'award.tasks.start_point_awadr_task',
         'schedule': crontab(hour=6, minute=10, day_of_week=5)
     },
 }
